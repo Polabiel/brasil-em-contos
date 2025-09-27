2b707c09c639fd975c9c303f52180efd5481975b
Polabiel <bielgabrieloliveira77@gmail.com>
2025-09-27 20:06:42 -0300

    feat(env): add optional ADMIN_DISCORD_IDS environment variable

--- src/env.js
+++ src/env.js
@@ -22,6 +22,7 @@ export const env = createEnv({
     EMAIL_SERVER_HOST: z.string(),
     EMAIL_SERVER_PORT: z.string(),
     EMAIL_FROM: z.string().email(),
+    ADMIN_DISCORD_IDS: z.string().optional(),
   },
 
   /**
@@ -48,6 +49,7 @@ export const env = createEnv({
     EMAIL_SERVER_HOST: process.env.EMAIL_SERVER_HOST,
     EMAIL_SERVER_PORT: process.env.EMAIL_SERVER_PORT,
     EMAIL_FROM: process.env.EMAIL_FROM,
+  ADMIN_DISCORD_IDS: process.env.ADMIN_DISCORD_IDS,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
